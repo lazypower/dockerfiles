cca8d92a2f0c58a756b8c5ede2118b79e04ccfc7
Charles Butler <chuck@dasroot.net>
2017-06-23 23:13:45 -0500

    Implement permission templates, rename to gotempl

--- discord-musicbot/start.sh
+++ discord-musicbot/start.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
-/usr/local/bin/env-template /templates/options.ini.j2 > /MusicBot/config/options.ini
+/usr/local/bin/env-template /templates/options.ini.gotempl > /MusicBot/config/options.ini
+/usr/local/bin/env-template /templates/permissions.ini.gotempl > /MusicBot/config/permissions.ini
 
 python3 run.py
