3b7d506742cb317500592e4dc52d7e188cdf0f02
Charles Butler <chuck@dasroot.net>
2017-08-13 04:29:24 -0500

    Adds mountpoint specific entries because this was harder than it needed to be

--- icecast/radio.sh
+++ icecast/radio.sh
@@ -5,5 +5,7 @@ set -e
 sed -i "s/<source-password>hackme/<source-password>$SOURCE_PASSWORD/" /etc/icecast/icecast.xml
 sed -i "s/<relay-password>hackme/<relay-password>$RELAY_PASSWORD/" /etc/icecast/icecast.xml
 sed -i "s/<admin-password>hackme/<admin-password>$ADMIN_PASSWORD/" /etc/icecast/icecast.xml
+# this is for the mountpoint configuration
+sed -i "s/<password>hackmemore/<password>$SOURCE_PASSWORD/" /etc/icecast/icecast.xml
 
 icecast -c /etc/icecast/icecast.xml
