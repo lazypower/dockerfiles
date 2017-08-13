57207a8896da5ecc55414e1a385b1ec60bcda4bf
Charles Butler <chuck@dasroot.net>
2017-08-13 03:35:44 -0500

    correct typo in icecast startup template editor

--- icecast/radio.sh
+++ icecast/radio.sh
@@ -3,7 +3,7 @@
 set -e
 
 sed -i "s/<source-password>hackme/<source-password>$SOURCE_PASSWORD/" /etc/icecast/icecast.xml
-sed -i "s/<relay-password>hackme/<relay-password>$RElAY_PASSWORD/" /etc/icecast/icecast.xml
+sed -i "s/<relay-password>hackme/<relay-password>$RELAY_PASSWORD/" /etc/icecast/icecast.xml
 sed -i "s/<admin-password>hackme/<admin-password>$ADMIN_PASSWORD/" /etc/icecast/icecast.xml
 
 icecast -c /etc/icecast/icecast.xml
