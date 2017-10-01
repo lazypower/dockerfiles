2c6941a34c29e62c5b3975b8ae45c4facbfb32b3
Charles Butler <chuck@dasroot.net>
2017-10-01 04:10:30 -0500

    Initial commit of the failed pod garbage collector
    This is not intended to be fabulous, or as a great idea, but it will
    however fix a problem with JLNP containers being left around because
    they error on termination.

---
+++ failed-jenkins-pod-reaper/scanner.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -eu
+
+SLEEP="${SLEEP:-300}"
+NAMESPACE="${NAMESPACE:-default}"
+
+scan() {
+    # Read the kube token from the system
+    KUBE_TOKEN=$(</var/run/secrets/kubernetes.io/serviceaccount/token)
+
+    if [ -z "${KUBE_TOKEN}" ]; then
+        echo "Missing Kube Token. Run this pod in a namespace with service accounts enabled."
+        exit 1
+    fi
+
+    # Query the API and prepare the result to feed into JQ
+    json=$(curl -sSk -H "Authorization: Bearer ${KUBE_TOKEN}" https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_PORT_443_TCP_PORT}/api/v1/namespaces/${NAMESPACE}/pods)
+
+    # Sift through the data and find only the names of failed pods
+    located_pods=$(echo ${json} | jq '.items[] | {"name": .metadata.name, "status": .status.phase} | select(.status == "Failed") | .name ')
+
+    # Timestamp when something happened for auditing purposes...
+    date
+    echo $located_pods | xargs kubectl delete po
+}
+
+# Main run loop
+while :
+do
+    scan
+    sleep $SLEEP
+done
