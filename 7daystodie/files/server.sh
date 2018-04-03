2213fcaa2407ce50bdbe69a00a797f00bdb45c3a
Charles Butler <chuck@linuxlab.sh>
2018-04-02 22:13:11 -0500

    Bump 7days server init

--- 7daystodie/files/server.sh
+++ 7daystodie/files/server.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+set -ex
+
 export LD_LIBRARY_PATH=$(dirname $SDTD_DIR)
+
+export ADMIN_CONFIG="${SDTD_DIR}/Saved/serveradmin.xml"
 
 if [ -n "STEAMCMD_APP_BETA" ]; then
   BETA="-beta $STEAMCMD_APP_BETA"
+fi
+
+if [ ! -f $ADMIN_CONFIG ]; then
+    if [ -f /config/serveradmin.xml ]; then
+	mkdir -p dirname $ADMIN_CONFIG
+	cp /config/serveradmin.xml $ADMIN_CONFIG
+    fi
 fi
 
 steamcmd.sh +login anonymous +force_install_dir $SDTD_DIR +app_update $STEAM_APP_ID $BETA +quit
